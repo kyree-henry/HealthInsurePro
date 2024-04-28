db0337505bae0bb16455b7f55913bf275d31654e
Kyree henry <ene.henry.eh@gmail.com>
2024-04-28 03:24:11 +0100

    Commit 006 * Added gitIgnore file * Added IdentityUser and IdentityRole extended entity * Added HealthCheck * Added Application Entities ( Claims, Expenses, PolicyHolder )

---
+++ src/HealthInsurePro/Domain/Identity/ApplicationRole.cs
@@ -0,0 +1,16 @@
+﻿namespace HealthInsurePro.Domain.Identity
+{
+    public class ApplicationRole : IdentityRole<Guid>
+    {
+        public ApplicationRole()
+        { }
+
+        public ApplicationRole(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+
+        public string Description { get; set; }
+    }
+}
