e059c6e4accb8942b13cfdd70ad0aab31e4919d7
Kyree henry <ene.henry.eh@gmail.com>
2024-04-29 14:14:26 +0100

    Commit 034 * Added Roles to Token

--- src/Application/Abstracts/Services/ITokenService.cs
+++ src/Application/Abstracts/Services/ITokenService.cs
@@ -4,6 +4,6 @@ namespace HealthInsurePro.Application.Abstracts.Services
 {
     public interface ITokenService
     {
-        Task<string> CreateToken(ApplicationUser user);
+        Task<string> CreateToken(ApplicationUser user, IList<string>? roles = null);
     }
 }
